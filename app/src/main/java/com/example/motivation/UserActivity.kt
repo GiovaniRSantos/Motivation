e79755ae3f85ba58c0b48f2aeeb3f03951642fce
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-07 20:17:38 -0300

    tela inicial

---
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -0,0 +1,28 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+        supportActionBar?.hide()
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_save) {
+
+        }
+    }
+}
