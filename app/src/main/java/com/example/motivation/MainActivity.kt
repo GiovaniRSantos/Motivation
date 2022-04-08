b634fc6373e8a1a677d69b0c20f17e1a74635f6e
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-07 21:06:54 -0300

    SharedPreferences

--- app/src/main/java/com/example/motivation/MainActivity.kt
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.motivation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,7 @@ import com.example.motivation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         supportActionBar?.hide()
 
+        handleUserName()
+
         binding.buttonNewPhrase.setOnClickListener(this)
     }
 
@@ -23,5 +27,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (view.id == R.id.button_new_phrase) {
             var s = ""
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun handleUserName() {
+
+        val name = SecurityPreferences(this).getString("USER_NAME")
+
+        binding.textSalutation.text = "Olá, ${name}!"
     }
 }
