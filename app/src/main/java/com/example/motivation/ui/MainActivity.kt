f361985691173e1e2e37b713397d46d7b425c59f
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-08 10:48:19 -0300

    setColorFilter

--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.motivation.MotivationConstants
 import com.example.motivation.R
 import com.example.motivation.SecurityPreferences
@@ -24,11 +25,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         handleUserName()
 
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
+
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
             var s = ""
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
         }
     }
 
@@ -38,5 +45,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
 
         binding.textSalutation.text = "Olá, ${name}!"
+    }
+
+    private fun handleFilter(id: Int) {
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+
+        if (id == R.id.image_all) {
+            binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        } else if (id == R.id.image_happy) {
+            binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        } else {
+            binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        }
     }
 }
